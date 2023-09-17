bed581e6ca22cab5f4a2635bf3857457a949d48c
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-09-17 18:16:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Velikost okna
+
+// Zjistěte, jak velké je okno stránky a prohlížeče.
+
+// Pohrajte si s vlastnostmi window.innerWidth a window.innerHeight a zjistěte, jak velké je okno stránky. Vypište tyto hodnoty do stránky a zkuste stránky několikrát změnšít a zvětšit, Pozor, že je vždy potřeba udělat refresh stránky, aby se hodnoty změnily.
+
+// Vypište do stránky také vlastnosti window.outerWidth a window.outerHeight. Opět si pohrajte s velíkostí okna prohlížeče a pozorujte, jak se tyto hodnoty mění.
